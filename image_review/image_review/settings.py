c49f9e7610138f5b7dca61ca4fab2a96b5739ff1
Matt Poepping <matt.poepping@gmail.com>
2024-09-11 08:37:48 -0600

    p

--- image_review/image_review/settings.py
+++ image_review/image_review/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "image_review.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": IMAGES_DIR / "db.sqlite3",
     }
 }
 
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "MST"
 
 USE_I18N = True
 
